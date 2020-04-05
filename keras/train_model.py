e7d5f7b6fc381932b33aa4b7444ad5a468d04624
Kevin_Xie <kevin.xie@wustl.edu>
2020-04-05 11:08:28 -0500

    one-hot encoded the data generators

--- keras/train_model.py
+++ keras/train_model.py
@@ -1,9 +1,13 @@
 """
 Trains a given model on training and testing set directories
 Uses https://keras.io/preprocessing/image/ as a documentation guide
-And https://github.com/zhixuhao/unet/blob/master/data.py as a guide on how to use the ImageDataGenerator for UNET
+And https://github.com/zhixuhao/unet/blob/master/data.py and 
+https://github.com/advaitsave/Multiclass-Semantic-Segmentation-CamVid/blob/master/Multiclass%20Semantic%20Segmentation%20using%20U-Net.ipynb
+as a guide on how to use the ImageDataGenerator for UNET
 """
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from one_hot_encoder import encode_image
+import numpy as np
 
 def train_model(model, training_directory, validation_directory,\
                 epochs=10, steps_per_epoch=1000, validation_steps=100):
@@ -14,41 +18,53 @@ def train_model(model, training_directory, validation_directory,\
     #--------------------- Image Preprocessing ---------------------#
     
     #Data augmentation generators
-    segmentation_datagen = ImageDataGenerator(
+    train_image_generator = ImageDataGenerator(
         rescale=1./255,
         shear_range=0.2,
         zoom_range=0.2,
         horizontal_flip=True)
+    train_mask_generator = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+    val_image_generator = ImageDataGenerator(rescale=1./255);
+    val_mask_generator = ImageDataGenerator(rescale=1./255);
     
-    #apply data augmentation for training dataset
-    train_image_datagen = segmentation_datagen.flow_from_directory(
+    #apply data augmentation for training dataset and validation dataset
+    train_image_datagen = train_image_generator.flow_from_directory(
         training_directory,
         target_size=input_shape,
         classes=["images"],
         class_mode=None,
         seed=0)
-    train_mask_datagen = segmentation_datagen.flow_from_directory(
+    train_mask_datagen = train_mask_generator.flow_from_directory(
         training_directory,
         target_size=input_shape,
         classes=["masks"],
         class_mode=None,
         seed=0)
-    train_datagen = zip(train_image_datagen, train_mask_datagen)
-    
-    #apply data augmentation for validation set
-    val_image_datagen = segmentation_datagen.flow_from_directory(
+    val_image_datagen = val_image_generator.flow_from_directory(
         validation_directory,
         target_size=input_shape,
         classes=["images"],
         class_mode=None,
         seed=0)
-    val_mask_datagen = segmentation_datagen.flow_from_directory(
+    val_mask_datagen = val_mask_generator.flow_from_directory(
         validation_directory,
         target_size=input_shape,
         classes=["masks"],
         class_mode=None,
         seed=0)
-    val_datagen = zip(val_image_datagen, val_mask_datagen)
+    
+    #one hot encode the training masks and condense the training data
+    train_mask_encoded_datgen = np.asarray([encode_image(mask) for mask in train_mask_datagen])
+    train_datagen = zip(train_image_datagen, train_mask_encoded_datgen)
+    
+    #one hot encode the validation masks and condense the validation data
+    val_mask_encoded_datagen = np.asarray([encode_image(mask) for mask in val_mask_datagen])
+    val_datagen = zip(val_image_datagen, val_mask_encoded_datagen)
+    
     
     #--------------------- Begin Training ---------------------#
     
@@ -56,8 +72,8 @@ def train_model(model, training_directory, validation_directory,\
         train_datagen,
         steps_per_epoch=steps_per_epoch,
         epochs=epochs,
-        validation_data=validation_steps,
-        validation_steps=100)
+        validation_data=val_datagen,
+        validation_steps=validation_steps)
     
     return model
     
