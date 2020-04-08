6fa03227dc45f51dab10857d2a27f1be25405a72
Kevin_Xie <kevin.xie@wustl.edu>
2020-04-08 14:12:58 -0500

    Update main.py

--- keras/main.py
+++ keras/main.py
@@ -7,8 +7,8 @@ from train_model import train_model
 #-------------------------- Parameter Definitions ---------------------------#
 
 #filepaths to where the training, validation and testing images & masks are
-training_directory = ''
-validation_directory = ''
+training_directory = r'C:\Users\Kevin Xie\Desktop\MS Spring Respositories\514A\CSE514_Project\data\Training'
+validation_directory = r'C:\Users\Kevin Xie\Desktop\MS Spring Respositories\514A\CSE514_Project\data\Validation'
 testing_directory = ''
 
 #do we need to create the datasets inside the directories listed above?
@@ -19,33 +19,28 @@ num_classes = 20
 input_size = (572, 572, 3)
 optimizer = "adam"
 learning_rate = 1e-3
-epochs = 10
-steps_per_epoch = 1000
-validation_steps = 100
+epochs = 5
+steps_per_epoch = 10
+validation_steps = 10
+batch_size = 1;
 
-#PLAY AROUND WITH THE ONE HOT ENCODER
-import matplotlib as mpl
-from one_hot_encoder import encode_image, decode_encoded_image
-image = mpl.pyplot.imread(r'C:\Users\Kevin Xie\Desktop\MS Spring Respositories\514A\Data\SegmentationClass/2007_000762.png')
-encode = encode_image(image)
-decode = decode_encoded_image(encode)
+#---------------------------- Dataset Generation ----------------------------#
 
 # =============================================================================
-# #---------------------------- Dataset Generation ----------------------------#
-# 
 # if needs_dataset_generation:
 #     createDatasets()
 # 
-# #------------------------------ Model Training ------------------------------#
-# 
-# 
-# #create a U_Net model
-# unet = generate_u_net(num_class=20, input_size=(572,572,3), \
-#                       optimizer="adam", learning_rate=1e-3)
-# #train u-net
-# unet = train_model(unet, training_directory, validation_directory, \
-#                    epochs, steps_per_epoch, validation_steps)
-#     
-#     
-# 
 # =============================================================================
+
+#------------------------------ Model Training ------------------------------#
+
+
+#create a U_Net model
+unet = generate_u_net(num_classes=21, input_size=input_size, \
+                      optimizer="adam", learning_rate=1e-3)
+#train u-net
+unet = train_model(unet, training_directory, validation_directory, \
+                   epochs, steps_per_epoch, validation_steps, batch_size = batch_size)
+    
+    
+
